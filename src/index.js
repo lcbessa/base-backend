5b6282031c56e8dc5028a3cc2815d1ac8f180a50
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2021-10-24 14:35:30 -0300

    Teste com requisição Post

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-import express from "express";
-import { Router } from "express";
+import express from 'express';
+import { Router } from 'express';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json()); // padronização de troca de dados
-app.use(express.urlencoded({extended: false})); // segurança
+app.use(express.urlencoded({extended: false})); //  
 
 const router = new Router();
 
 router.get('/',(req,res) => {
-    return res.status(200).send({message})
+    return res.status(200).send({message: 'Hello World!'});
 });
 
+router.post('/teste',(req,res) => {
+    return res.status(200).send({dados: req.body});
+});
+
+app.use(router);
+
 console.log(`Servidor rodando no link http://localhost:${port}`);
-app.listen(port);
+app.listen(port); // abrir o servidor
 
